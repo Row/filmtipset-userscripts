d525a82f534159c4056d196d6e668711d5f207e4
Jon <jon@powha.net>
2014-11-01 14:15:06 +0100

    fix update to work with new version of imdb and ft, fix Chrome support.

--- filmtipset-cast-images.user.js
+++ filmtipset-cast-images.user.js
@@ -1,10 +1,8 @@
 // ==UserScript==
 // @name           Filmtipset Cast Images
 // @namespace      https://github.com/Row/filmtipset-userscripts
-// @description    Displays small cast images on each movie page on filmtipset.se
-//                 Firefox about:config network.http.sendRefererHeader must be set to 1,
-//                 or else imdb's image server will block your requests.
-// @version        0.04
+// @description    Displays small actor images on each movie page on filmtipset.se
+// @version        0.1
 // @include        http://www.filmtipset.se/film/*
 // @include        http://nyheter24.se/filmtipset/film/*
 // ==/UserScript==
@@ -15,17 +13,31 @@ function get(url, cb)
 {
   GM_xmlhttpRequest({
      method: "GET",
+     headers: {
+        "Referer": ""
+     },
      url: url,
-     onload: function(xhr) { cb( xhr.responseText); }
+     onload: function(xhr) { cb( xhr.responseText); },
+     overrideMimeType: 'text/plain; charset=x-user-defined'
   });
 }
 
-function setCastStyle(el,img)
+function fetchImageData(el, imdbObj)
+{
+    get(imdbObj.img, function(bin) {
+        var base64img = customBase64Encode(bin);
+        setCastStyle(el, 'data:image/jpeg;base64,' + base64img);
+    });
+}
+
+function setCastStyle(el, img)
 {
     el.style.display    = 'block';
-    el.style.padding    = '9px 5px 9px 50px';
+    el.style.padding    = '14px 5px 14px 50px';
     el.style.margin     = '2px 0';
-    el.style.background = '#F3F4E7 url('+img+') no-repeat 10px 50%';
+    el.style.float      = 'left';
+    el.style.width      = '120px';
+    el.style.background = '#F3F4E7 url("'+img+'") no-repeat 10px 50%';
 }
 
 function drawCastBox()
@@ -49,15 +61,15 @@ function drawCastBox()
 function drawImages(text)
 {
     var imdbActors = [];
-    text.replace(/title="(.*?)"\n\n\s+src="(.*?)" class=""/gm,
-            function(m, n, o) {imdbActors.push({img: o, name: n})}
+    text.replace(/title="(.*?)".*?loadlate="(.*?)"/gm,
+        function(m, n, o) {imdbActors.push({img: o, name: n})}
     );
     for(var i = 0; i < persons.snapshotLength; i++) {
         for(var j = 0; j < imdbActors.length; j++) {
             var ftActor = persons.snapshotItem(i).text.replace(/ \(.+\)/,'').toLowerCase();
             var imdbActor = imdbActors[j].name.toLowerCase().replace(/\./gm,'');
             if(imdbActor == ftActor) {
-                setCastStyle(persons.snapshotItem(i), imdbActors[j].img);
+                fetchImageData(persons.snapshotItem(i), imdbActors[j]);
             }
         }
     }
@@ -69,8 +81,75 @@ function callImdb()
     var imdbUrl = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
     if(!imdbUrl || !drawCastBox())
         return false;
-
     get(imdbUrl.toString(), drawImages);
+}
+
+/* http://stackoverflow.com/questions/8778863/downloading-an-image-using-xmlhttprequest-in-a-userscript */
+function customBase64Encode (inputStr) 
+{
+    var
+        bbLen               = 3,
+        enCharLen           = 4,
+        inpLen              = inputStr.length,
+        inx                 = 0,
+        jnx,
+        keyStr              = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+                            + "0123456789+/=",
+        output              = "",
+        paddingBytes        = 0;
+    var
+        bytebuffer          = new Array (bbLen),
+        encodedCharIndexes  = new Array (enCharLen);
+
+    while (inx < inpLen) {
+        for (jnx = 0;  jnx < bbLen;  ++jnx) {
+            /*--- Throw away high-order byte, as documented at:
+              https://developer.mozilla.org/En/Using_XMLHttpRequest#Handling_binary_data
+            */
+            if (inx < inpLen)
+                bytebuffer[jnx] = inputStr.charCodeAt (inx++) & 0xff;
+            else
+                bytebuffer[jnx] = 0;
+        }
+
+        /*--- Get each encoded character, 6 bits at a time.
+            index 0: first  6 bits
+            index 1: second 6 bits
+                        (2 least significant bits from inputStr byte 1
+                         + 4 most significant bits from byte 2)
+            index 2: third  6 bits
+                        (4 least significant bits from inputStr byte 2
+                         + 2 most significant bits from byte 3)
+            index 3: forth  6 bits (6 least significant bits from inputStr byte 3)
+        */
+        encodedCharIndexes[0] = bytebuffer[0] >> 2;
+        encodedCharIndexes[1] = ( (bytebuffer[0] & 0x3) << 4)   |  (bytebuffer[1] >> 4);
+        encodedCharIndexes[2] = ( (bytebuffer[1] & 0x0f) << 2)  |  (bytebuffer[2] >> 6);
+        encodedCharIndexes[3] = bytebuffer[2] & 0x3f;
+
+        //--- Determine whether padding happened, and adjust accordingly.
+        paddingBytes          = inx - (inpLen - 1);
+        switch (paddingBytes) {
+            case 1:
+                // Set last character to padding char
+                encodedCharIndexes[3] = 64;
+                break;
+            case 2:
+                // Set last 2 characters to padding char
+                encodedCharIndexes[3] = 64;
+                encodedCharIndexes[2] = 64;
+                break;
+            default:
+                break; // No padding - proceed
+        }
+
+        /*--- Now grab each appropriate character out of our keystring,
+            based on our index array and append it to the output string.
+        */
+        for (jnx = 0;  jnx < enCharLen;  ++jnx)
+            output += keyStr.charAt ( encodedCharIndexes[jnx] );
+    }
+    return output;
 }
 
 callImdb();
